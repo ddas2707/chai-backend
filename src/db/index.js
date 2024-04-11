45af968950432ab0486a532046141c75342a53af
DHRUB DAS <dhrub27das@gmail.com>
2024-04-11 13:45:11 +0530

    db conn

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+
+const connectDB = async () => {
+    try {
+        //console.log(`${process.env.MONGODB_URI}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        //console.log('hetty')
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
